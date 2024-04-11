7339a579ae39ce91bc40f2e01da75c41748ec4a7
Rafa Albert <albertc.rafaela@gmail.com>
2024-04-11 18:10:56 -0300

    migrate project to react

--- README.md
+++
@@ -1,3 +0,0 @@
-Artemis I launch countdown made with JavaScript for the purpose of exercise.
-
-[Preview](https://albert-rafa.github.io/artemis-countdown/)
---
+++ src/app.tsx
@@ -0,0 +1,44 @@
+import artemisLogo from "./assets/artemis-logo.png";
+import { Countdown } from "./components/countdown";
+import { Link } from "./components/link"
+import { Social } from "./components/social";
+
+export default function App() {
+  const launch = new Date(Date.UTC(2025, 7, 1, 0, 1))
+  // Semptember 1st, 2025 00:01 GMT (not confirmed)
+
+  return (
+    <div className="text-white h-full w-full bg-image bg-cover flex flex-col justify-center items-center gap-6">
+      <div className="flex flex-col justify-center items-center">
+        <img 
+          src={artemisLogo} 
+          alt="Artemis program" 
+          className="h-[20vh]" 
+        />
+        <p className="font-display text-4xl">
+          <span className="font-medium">Artemis 2</span> launch in
+        </p>
+      </div>
+
+      <Countdown date={launch} />
+
+      <div className="flex flex-col gap-3">
+        <div className="flex gap-2 text-xl uppercase">
+          <Link 
+            href="https://www.nasa.gov/feature/artemis/"
+            target="_blank"
+          >
+            Learn more
+          </Link>
+          <Link 
+            href="https://youtube.com"
+            target="_blank"
+          >
+            Watch live
+          </Link>
+        </div>
+        <Social />
+      </div>
+    </div>
+  )
+}
